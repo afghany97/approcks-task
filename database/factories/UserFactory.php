435bdeea17db4a83fc7d9be751445a0e7aa26bbf
afghany <muhammadreda97@gmail.com>
2019-06-23 01:04:05 +0200

    create model factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,10 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Board;
+use App\Issue;
+use App\Record;
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -23,5 +27,29 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Board::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'user_id' => factory(User::class)->create()->id
+    ];
+});
+
+$factory->define(Record::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'board_id' => factory(Board::class)->create()->id
+    ];
+});
+
+$factory->define(Issue::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->sentence,
+        'record_id' => factory(Record::class)->create()->id,
+        'user_id' => factory(User::class)->create()->id,
+        'deadline' => $faker->date()
     ];
 });
